d0c0a994a5f222fa91e9f1671a6b625e8dbe0b5b
Your Name <you@example.com>
2022-04-03 11:08:42 -0500

    first commit

---
+++ Bonce.py
@@ -0,0 +1,62 @@
+#Usted usa Python evironment
+import os
+
+
+os.system('cd')
+os.system('clear')
+os.system('pkg install nano')
+os.system('pkg install git')
+os.system('pkg install figlet')
+os.system('pkg install toilet')
+os.system('rm -rf /data/data/com.termux/files/usr/etc/motd-playstore')
+os.system('cd')
+os.system('rm -rf /data/data/com.termux/files/usr/etc/motd')
+os.system('cd')
+
+while(True):
+    print('---SUPER MENU---')
+    print('[1]-Elegir-LOVE-')
+    print('[2]-Elegir-DarRosa-')
+    print('[3]-Elegir-Calaberita-')
+    print('[4]-Elegir-Perrito-')
+    print('[5]-Elegir-Conejito-')
+    print('[6]-Elegir-Robot-')
+    print('[7]-Elegir-SuperPc-')
+    print('[8]-Elegir-GatoTanke-')
+    print('[9]-Elegir-UNAflor-')
+    print('[10]-Elegir-Calabitaciga-')
+    print('[11]-Elegir-PersonaTanke-')
+    print('[12]-Elegir-Gato-')
+    print('[13]-Elegir-TEAMO-')
+    print('[14]-SALIR-')
+    respuesta=input('Elige la opción que le agrado: ')
+    if respuesta=='1':
+        os.system('cp -i /data/data/com.termux/files/home/Programa/1/bash.bashrc /data/data/com.termux/files/usr/etc')
+    elif respuesta=='2':
+        os.system('cp -i /data/data/com.termux/files/home/Programa/2/bash.bashrc /data/data/com.termux/files/usr/etc')
+    elif respuesta=='3':
+        os.system('cp -i /data/data/com.termux/files/home/Programa/3/bash.bashrc /data/data/com.termux/files/usr/etc')
+    elif respuesta=='4':
+        os.system('cp -i /data/data/com.termux/files/home/Programa/4/bash.bashrc /data/data/com.termux/files/usr/etc')
+    elif respuesta=='5':
+        os.system('cp -i /data/data/com.termux/files/home/Programa/5/bash.bashrc /data/data/com.termux/files/usr/etc')
+    elif respuesta=='6':
+        os.system('cp -i /data/data/com.termux/files/home/Programa/6/bash.bashrc /data/data/com.termux/files/usr/etc')
+    elif respuesta=='7':
+        os.system('cp -i /data/data/com.termux/files/home/Programa/7/bash.bashrc /data/data/com.termux/files/usr/etc')
+    elif respuesta=='8':
+        os.system('cp -i /data/data/com.termux/files/home/Programa/8/bash.bashrc /data/data/com.termux/files/usr/etc')
+    elif respuesta=='9':
+        os.system('cp -i /data/data/com.termux/files/home/Programa/9/bash.bashrc /data/data/com.termux/files/usr/etc')
+    elif respuesta=='10':
+        os.system('cp -i /data/data/com.termux/files/home/Programa/10/bash.bashrc /data/data/com.termux/files/usr/etc')
+    elif respuesta=='11':
+        os.system('cp -i /data/data/com.termux/files/home/Programa/12/bash.bashrc /data/data/com.termux/files/usr/etc')
+    elif respuesta=='13':
+        os.system('cp -i /data/data/com.termux/files/home/Programa/13/bash.bashrc /data/data/com.termux/files/usr/etc')
+    elif respuesta=='14':
+        print('Esto es el fin')'
+        break
+    else:
+        print('Error elegiste una opción invalida')
+print('Gracias por realizar el cambio')
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Coprod
+# Goto
